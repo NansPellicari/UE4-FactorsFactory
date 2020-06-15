5936baa35bc27db22dc83c2dd060f07b9a77de91
Nans Pellicari <nans.pellicari@gmail.com>
2020-06-15 21:01:37 +0200

    Add license headers in every files

--- Source/NansFactorsFactoryUE4/Private/NansFactorsFactoryUE4.cpp
+++ Source/NansFactorsFactoryUE4/Private/NansFactorsFactoryUE4.cpp
@@ -1,4 +1,16 @@
-// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+// Copyright 2020-present Nans Pellicari (nans.pellicari@gmail.com).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include "NansFactorsFactoryUE4.h"
 
@@ -8,16 +20,16 @@
 
 void FNansFactorsFactoryUE4Module::StartupModule()
 {
-    // create your factory and shared pointer to it.
-    TSharedPtr<FNFactorSettingsPinFactory> Factory = MakeShareable(new FNFactorSettingsPinFactory());
-    // and now register it.
-    FEdGraphUtilities::RegisterVisualPinFactory(Factory);
+	// create your factory and shared pointer to it.
+	TSharedPtr<FNFactorSettingsPinFactory> Factory = MakeShareable(new FNFactorSettingsPinFactory());
+	// and now register it.
+	FEdGraphUtilities::RegisterVisualPinFactory(Factory);
 }
 
 void FNansFactorsFactoryUE4Module::ShutdownModule()
 {
-    // This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
-    // we call this function before unloading the module.
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
 }
 
 #undef LOCTEXT_NAMESPACE
--- generate-image.js
+++ generate-image.js
@@ -1,9 +1,23 @@
+// Copyright 2020-present Nans Pellicari (nans.pellicari@gmail.com).
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 const path = require("path");
 const fs = require("fs");
 const chalk = require("chalk");
 const { spawnSync } = require("child_process");
 
-// config list can be foud here: https://github.com/mermaid-js/mermaid/blob/master/docs/mermaidAPI.md#mermaidapi-configuration-defaults
+// config list can be found here: https://github.com/mermaid-js/mermaid/blob/master/docs/mermaidAPI.md#mermaidapi-configuration-defaults
 
 async function compileDiagram(file, format) {
 	const result = file.replace(".mmd", "." + format);
